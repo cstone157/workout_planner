b34a99cd349284aef8dfc39e16e04152b1d54ce9
cstone157 <c_stone157@hotmail.com>
2026-06-04 22:01:35 -0700

    Trying to fix the issues with the svelte project that claude generated.

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,13 +1,1 @@
-import adapter from '@sveltejs/adapter-auto';
-
-export default {
-  kit: {
-    adapter: adapter(),
-    alias: {
-      $components: 'src/components',
-      $lib: 'src/lib',
-      $stores: 'src/stores',
-      $api: 'src/api'
-    }
-  }
-};
+export default {};
